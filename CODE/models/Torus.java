a3a8f2e7939b2ca2e0b8bec515dc7f3e47b466db
elfaretazd <elfaretadewi@gmail.com>
2025-05-31 20:55:32 +0700

    Torus.java

--- CODE/models/Torus.java
+++ CODE/models/Torus.java
@@ -1,25 +1,38 @@
 package UAP.models;
 
 import UAP.bases.Shape;
-import UAP.interfaces.*;
+import UAP.interfaces.MassCalculable;
+import UAP.interfaces.MassConverter;
+import UAP.interfaces.PIRequired;
+import UAP.interfaces.ShippingCostCalculator;
+import UAP.interfaces.ThreeDimensional;
 
-public class Torus extends Shape implements ThreeDimensional, PIRequired, MassCalculable, MassConverter, ShippingCostCalculator {
+public class Torus extends Shape implements ThreeDimensional, MassCalculable, PIRequired, MassConverter, ShippingCostCalculator {
     private double majorRadius;
     private double minorRadius;
 
     public Torus() {
-        setName("Donat Torus");
+        super();
+        setName("Donat dengan lubang"); 
     }
 
     public Torus(double majorRadius, double minorRadius) {
-        this();
+        this(); 
         this.majorRadius = majorRadius;
         this.minorRadius = minorRadius;
     }
 
+    public double getMajorRadius() {
+        return majorRadius;
+    }
+
+    public double getMinorRadius() {
+        return minorRadius;
+    }
+
     @Override
     public double getVolume() {
-        return 2 * PI * PI * majorRadius * minorRadius * minorRadius;
+        return Math.ceil(2 * PI * PI * majorRadius * (minorRadius * minorRadius));
     }
 
     @Override
@@ -29,25 +42,27 @@ public class Torus extends Shape implements ThreeDimensional, PIRequired, MassCa
 
     @Override
     public double getMass() {
-        return getVolume() * DENSITY;
+        return DENSITY * getSurfaceArea() * THICKNESS;
     }
 
     @Override
-    public double gramToKilogram() {
-        return getMass() / DENOMINATOR;
+    public double gramToKilogram(double gram) {
+        return gram / DENOMINATOR;
     }
 
     @Override
-    public double calculateCost() {
-        return Math.ceil(gramToKilogram()) * PRICE_PER_KG;
+    public double calculateCost(double massInKg) {
+        return Math.ceil(massInKg) * PRICE_PER_KG;
     }
 
     @Override
     public void printInfo() {
-        System.out.printf("Volume          : %.2f\n", getVolume());
-        System.out.printf("Luas permukaan  : %.2f\n", getSurfaceArea());
-        System.out.printf("Massa           : %.2f\n", getMass());
-        System.out.printf("Massa dalam kg  : %.2f\n", gramToKilogram());
-        System.out.printf("Biaya kirim     : Rp%.0f\n", calculateCost());
+        System.out.println("=============================================");
+        System.out.println("Volume : " + getVolume()); // printInfo includes volume, surface area, and mass [cite: 6]
+        System.out.println("Luas permukaan : " + getSurfaceArea()); // printInfo includes volume, surface area, and mass [cite: 6]
+        System.out.println("Massa : " + getMass()); // printInfo includes volume, surface area, and mass [cite: 6]
+        System.out.println("Massa dalam kg : " + gramToKilogram(getMass()));
+        System.out.println("Biaya kirim : Rp" + (int) calculateCost(gramToKilogram(getMass()))); // Cast to int for output format [cite: 8]
+        System.out.println("=============================================");
     }
 }
