5767b54c278b6e52668acd9fae2c181be483f474
elfaretazd <elfaretadewi@gmail.com>
2025-05-31 19:51:30 +0700

    KalkulatorPabrik.java

---
+++ CODE/mains/KalkulatorPabrik.java
@@ -0,0 +1,44 @@
+package UAP.mains;
+
+import java.util.Scanner;
+import UAP.models.Torus;
+import UAP.models.Sphere;
+
+public class KalkulatorPabrik {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=============================================");
+        System.out.println("Kalkulator Pabrik Cetakan Donat Rumahan");
+        System.out.println("ELFARETA ZABRINA DEWI");
+        System.out.println("245150701111015");
+        System.out.println("=============================================");
+      
+        Torus torusDefault = new Torus();
+        System.out.println(torusDefault.getName());
+
+        System.out.println("=============================================");
+        System.out.print("Isikan Radius   : ");
+        double R = sc.nextDouble(); // e.g., 7
+        System.out.print("Isikan radius   : ");
+        double r = sc.nextDouble(); // e.g., 3.5
+
+        Torus torusInput = new Torus(R, r);
+        System.out.println("=============================================");
+        torusInput.printInfo();
+
+        System.out.println("=============================================");
+        System.out.println("Donat tanpa lubang");
+
+        Sphere sphereDefault = new Sphere();
+        System.out.println(sphereDefault.getName());
+
+        System.out.println("=============================================");
+        System.out.print("Isikan radius   : ");
+        double r2 = sc.nextDouble(); // e.g., 21
+
+        Sphere sphereInput = new Sphere(r2);
+        System.out.println("=============================================");
+        sphereInput.printInfo();
+        System.out.println("=============================================");
+    }
+}
